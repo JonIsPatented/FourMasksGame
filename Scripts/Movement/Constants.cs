a1701f1134dd1f0020f186e53fefe0811117a0bc
Fern Badenhop <fern.badenhop@gmail.com>
2026-07-10 18:21:41 -0400

    Added movement constants to store static movement parameters used across the movement states

---
+++ Scripts/Movement/Constants.cs
@@ -0,0 +1,9 @@
+
+namespace FourMasksGame.Scripts.Movement;
+
+public static class Constants
+{
+    public const float PLAYER_SPEED = 200f;
+    public const float PLAYER_JUMP_VELOCITY = -400f;
+    public const uint JUMP_PRESS_TOLERANCE = 5;
+}
