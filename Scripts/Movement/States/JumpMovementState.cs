8f328b39754f8c71257323a2f45d5b2aecee1f10
Fern Badenhop <fern.badenhop@gmail.com>
2026-07-10 18:26:00 -0400

    Wrote movement states run, idle, falling, and jump

---
+++ Scripts/Movement/States/JumpMovementState.cs
@@ -0,0 +1,34 @@
+
+using FourMasksGame.Scripts.Input;
+
+namespace FourMasksGame.Scripts.Movement.States;
+
+public class JumpMovementState : MovementState
+{
+    MovementStateLabel MovementState.Label => MovementStateLabel.Jump;
+
+    MovementState[] MovementState.FutureStates() => [
+        new FallingMovementState(),
+    ];
+
+    MovementDirective MovementState.Directive(MovementInfo info) => new()
+    {
+        playerControlsHorizontalVelocity = true,
+        horizontalMovementSpeed = Constants.PLAYER_SPEED,
+        impulseOnEnter = new(0f, Constants.PLAYER_JUMP_VELOCITY)
+    };
+
+    bool MovementState.CanEnter(MovementInfo info)
+    {
+        return info.grounded && InputManager.Instance.GetActionPressed("Jump", Constants.JUMP_PRESS_TOLERANCE);
+    }
+
+    bool MovementState.ShouldExitTo(MovementInfo info, MovementStateLabel futureState)
+    {
+        if (futureState == MovementStateLabel.Falling)
+        {
+            return info.realVelocity.Y < 0f;
+        }
+        return true;
+    }
+}
