cfec887cc989a87a50a15f154ef0565cbe6ce169
Fern Badenhop <fern.badenhop@gmail.com>
2026-07-10 16:29:06 -0400

    Created MovementInfo to inform the movement states.

---
+++ Scripts/Movement/MovementInfo.cs
@@ -0,0 +1,10 @@
+
+namespace FourMasksGame.Scripts.Movement;
+
+/// <summary>
+/// Information block for MovementState implementors.
+/// </summary>
+public struct MovementInfo
+{
+    public bool grounded;
+}
