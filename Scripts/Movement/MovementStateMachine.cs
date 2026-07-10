8ce1e348ee717e028d74285848fac56839d77d89
Fern Badenhop <fern.badenhop@gmail.com>
2026-07-10 16:50:16 -0400

    Wrote MovementStateMachine class to manage states for movement.

--- Scripts/Movement/MovementStateMachine.cs
+++ Scripts/Movement/MovementStateMachine.cs
@@ -5,7 +5,50 @@ namespace FourMasksGame.Scripts.Movement;
 public partial class MovementStateMachine : GodotObject
 {
     private MovementState currentState;
+    private MovementState[] futureStates = [];
+    private MovementInfo movementInfo = new();
 
     // An empty constructor is necessary for classes inheriting GodotObject.
-    public MovementStateMachine() {}
+    public MovementStateMachine() { }
+
+    public void PassInfo(MovementInfo info)
+    {
+        movementInfo = info;
+    }
+
+    public void Process()
+    {
+        currentState?.Process(movementInfo);
+        foreach (MovementState futureState in futureStates)
+        {
+            if (futureState.CanEnter(movementInfo))
+            {
+                if (currentState != null)
+                {
+                    if (currentState.ShouldExitTo(movementInfo, futureState))
+                    {
+                        TransitionToState(futureState);
+                    }
+                }
+                else
+                {
+                    TransitionToState(futureState);
+                }
+            }
+        }
+    }
+
+    public void EnterState(MovementState state)
+    {
+        TransitionToState(state);
+    }
+
+    private void TransitionToState(MovementState state)
+    {
+        currentState?.OnExit(movementInfo, state);
+        currentState = state;
+        futureStates = currentState?.FutureStates();
+        futureStates ??= [];
+        state?.OnEnter(movementInfo);
+    }
 }
