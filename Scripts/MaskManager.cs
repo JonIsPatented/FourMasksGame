2baac3ca042de590d67f9e092392110285fdad35
Fern Badenhop <fern.badenhop@gmail.com>
2026-07-11 00:18:32 -0400

    Mask Manager now stores mask table, singleton notified by ChangeMask method

--- Scripts/MaskManager.cs
+++ Scripts/MaskManager.cs
@@ -1,21 +1,30 @@
 using Godot;
+using System.Collections.Generic;
 
-namespace FourMasksGame.Scripts;
+namespace FourMasksGame.Scripts.Masks;
 
 public partial class MaskManager : Node
 {
     public static MaskManager Instance;
 
     [Signal]
-    public delegate void MaskChangedEventHandler();
+    public delegate void MaskChangedEventHandler(Mask mask);
 
-    public override void _Ready()
+    private readonly Dictionary<int, Mask> masks = new()
+    {
+        {0, ResourceLoader.Load<Mask>("res://Resources/Masks/wingMask.tres")},
+        {1, ResourceLoader.Load<Mask>("res://Resources/Masks/demonMask.tres")},
+        {2, ResourceLoader.Load<Mask>("res://Resources/Masks/mistMask.tres")},
+        {3, ResourceLoader.Load<Mask>("res://Resources/Masks/golemMask.tres")}
+    };
+
+    public MaskManager()
     {
         Instance = this;
     }
 
-    public void MaskChanged()
+    public void ChangeMask(int mask)
     {
-        EmitSignal(SignalName.MaskChanged);
+        EmitSignal(SignalName.MaskChanged, masks[mask]);
     }
 }
