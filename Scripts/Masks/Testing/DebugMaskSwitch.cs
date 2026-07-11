3b352e41afe408fe2c7cb71c3df4b968d1354750
Fern Badenhop <fern.badenhop@gmail.com>
2026-07-11 13:38:46 -0400

    Debug mask switch listens for input events instead of querying in process

--- Scripts/Masks/Testing/DebugMaskSwitch.cs
+++ Scripts/Masks/Testing/DebugMaskSwitch.cs
@@ -7,21 +7,20 @@ namespace FourMasksGame.Scripts.Masks.Testing;
 /// </summary>
 public partial class DebugMaskSwitch : Node
 {
-    public void _Process()
-    {
-        if (Input.IsActionJustPressed("DonMask1"))
+    public override void _Input(InputEvent @event) {
+        if (@event.IsActionPressed("DonMask1"))
         {
             MaskManager.Instance.ChangeMask(1);
         }
-        if (Input.IsActionJustPressed("DonMask2"))
+        if (@event.IsActionPressed("DonMask2"))
         {
             MaskManager.Instance.ChangeMask(2);
         }
-        if (Input.IsActionJustPressed("DonMask3"))
+        if (@event.IsActionPressed("DonMask3"))
         {
             MaskManager.Instance.ChangeMask(3);
         }
-        if (Input.IsActionJustPressed("DonMask4"))
+        if (@event.IsActionPressed("DonMask4"))
         {
             MaskManager.Instance.ChangeMask(4);
         }
