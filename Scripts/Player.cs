b8a6acfd221fc0d7a83e546b898fc95fe09703dd
Fern Badenhop <fern.badenhop@gmail.com>
2026-07-11 19:53:51 -0400

    Added AbilityBridge field to player and extracted movement directive interpretation to method

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,19 +1,23 @@
 using Godot;
 using FourMasksGame.Scripts.Movement;
 using FourMasksGame.Scripts.Input;
+using FourMasksGame.Scripts.Abilities;
 
 namespace FourMasksGame.Scripts;
 
 public partial class Player : CharacterBody2D
 {
     private MovementStateMachine movementStateMachine;
-    [Export] private Sprite2D sprite;
+    private AbilityBridge abilityBridge;
+
+    [Export] private AnimatedSprite2D sprite;
 
     // I don't think export variables are set until after _EnterTree and _Ready, so be careful about using exported node references here.
     public override void _EnterTree()
     {
         movementStateMachine = new();
         movementStateMachine.EnterState(new Movement.States.IdleMovementState());
+        abilityBridge = new();
     }
 
     public override void _Process(double delta)
@@ -34,7 +38,11 @@ public partial class Player : CharacterBody2D
     public override void _PhysicsProcess(double delta)
     {
         MovementDirective directive = movementStateMachine.GetDirective();
+        FollowMovementDirective(directive, delta);
+    }
 
+    public void FollowMovementDirective(MovementDirective directive, double delta)
+    {
         // For a character body, the Velocity property actually represents a requested velocity.
         // Without a reset, applications like gravity will pile up forever.
         // So I'm resetting each step here to the real velocity.
