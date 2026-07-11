2219778d824e291b89396fa962b269fb8b50d919
Fern Badenhop <fern.badenhop@gmail.com>
2026-07-11 12:25:10 -0400

    Created mask modulate remote control node to automatically modify modulate values for unlimited nodes

---
+++ Scripts/Environment/MaskModulateRemoteControlNode.cs
@@ -0,0 +1,55 @@
+using Godot;
+
+using FourMasksGame.Scripts.Masks;
+
+namespace FourMasksGame.Scripts.Environment;
+
+/// <summary>
+/// Responds to "mask changed" signals by adjusting the modulate value of itself and possibly additional targeted nodes.
+/// </summary>
+[GlobalClass]
+public partial class MaskModulateRemoteControlNode : Node2D
+{
+    private static Color defaultModulate = new(1, 1, 1);
+
+    /// <summary>
+    /// Additional targets to adjust the modulate value for. This node's modulate value is always adjusted, note that children will be affected by this node's modulate value automatically.
+    /// </summary>
+    [Export] private CanvasItem[] additionalTargets = [];
+
+    public override void _EnterTree()
+    {
+        MaskManager.Instance.MaskChanged += AdjustModulate;
+        AdjustModulate(MaskManager.Instance.CurrentMask);
+    }
+
+    private void AdjustModulate(Mask mask)
+    {
+        if (mask == null)
+        {
+            ResetModulate();
+            return;
+        }
+
+        foreach (CanvasItem modulateTarget in additionalTargets)
+        {
+            modulateTarget.Modulate = mask.EnvironmentColor;
+        }
+        Modulate = defaultModulate;
+    }
+
+    private void ResetModulate()
+    {
+        foreach (CanvasItem modulateTarget in additionalTargets)
+        {
+            modulateTarget.Modulate = defaultModulate;
+        }
+        Modulate = defaultModulate;
+    }
+
+    public override void _ExitTree()
+    {
+        MaskManager.Instance.MaskChanged -= AdjustModulate;
+        ResetModulate();
+    }
+}
