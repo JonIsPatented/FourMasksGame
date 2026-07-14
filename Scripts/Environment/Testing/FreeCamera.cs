1b21d7d8a471b086f595d7adfba03c82e153af5e
Fern Badenhop <fern.badenhop@gmail.com>
2026-07-13 23:29:11 -0400

    Free camera now uses WASD directly

--- Scripts/Environment/Testing/FreeCamera.cs
+++ Scripts/Environment/Testing/FreeCamera.cs
@@ -7,8 +7,13 @@ public partial class FreeCamera : Camera2D
 
     public override void _Process(double delta)
     {
-        Vector2 aimVector = Input.GetVector("AimLeft", "AimRight", "AimUp", "AimDown");
+        Vector2 aimVector = Vector2.Zero;
+        aimVector += Input.IsKeyPressed(Key.A) ? Vector2.Left: Vector2.Zero;
+        aimVector += Input.IsKeyPressed(Key.D) ? Vector2.Right : Vector2.Zero;
+        aimVector += Input.IsKeyPressed(Key.W) ? Vector2.Up : Vector2.Zero;
+        aimVector += Input.IsKeyPressed(Key.S) ? Vector2.Down : Vector2.Zero;
         Transform2D transform = Transform;
         transform.Origin = Transform.Origin + aimVector * Speed * (float)delta;
+        Transform = transform;
     }
 }
