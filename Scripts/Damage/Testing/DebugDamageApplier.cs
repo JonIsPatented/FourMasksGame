6080533b61df63eaa717525daee3010ac8ef8dfc
Fern Badenhop <fern.badenhop@gmail.com>
2026-07-15 12:17:14 -0400

    Updated damage debug scripts to make use of labels

--- Scripts/Damage/Testing/DebugDamageApplier.cs
+++ Scripts/Damage/Testing/DebugDamageApplier.cs
@@ -5,14 +5,15 @@ namespace FourMasksGame.Scripts.Damage.Testing;
 public partial class DebugDamageApplier : Node2D
 {
     [Export] DamageSender damageSender;
+    [Export] float damage = 1f;
 
     public override void _Input(InputEvent inputEvent)
     {
         if (inputEvent is InputEventKey keyEvent)
         {
-            if (keyEvent.Keycode == Key.Ctrl)
+            if (keyEvent.Keycode == Key.Ctrl && keyEvent.IsPressed() && !keyEvent.IsEcho())
             {
-                damageSender.Load(new DamageSource.Attack(0f, 0.5f, 1f, 1));
+                damageSender.Load(new DamageSource.Attack(0f, 0.5f, damage, 1));
             }
         }
     }
